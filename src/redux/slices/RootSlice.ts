d85844e0488804655d19d226d54d9e38e2c1b21e
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-03-26 15:38:24 -0600

    added some TS to rootSlice

--- src/redux/slices/RootSlice.ts
+++ src/redux/slices/RootSlice.ts
@@ -1,21 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface RootState {
+    year: string;
+    color: string;
+    make: string;
+    model: string;
+}
+
+const initialState: RootState = {
+    year: "year",
+    color: "color",
+    make: "make",
+    model: "model",
+};
 
 const rootSlice = createSlice({
-    name: 'root',
-    initialState: {
-        year: 'year',
-        color: 'color',
-        make: 'make',
-        model: 'model',
-    },
+    name: "root",
+    initialState,
     reducers: {
         // action is submitted elsewhere - written to state.name
-        chooseYear: (state, action) => {state.year = action.payload}, //All we're doing is setting the input to the state.year
-        chooseColor: (state, action) => { state.color = action.payload },
-        chooseMake: (state, action) => { state.make = action.payload },
-        chooseModel: (state, action) => {state.model = action.payload},
-    }
-})
+        chooseYear: (state, action: PayloadAction<string>) => {
+            state.year = action.payload;
+        }, //All we're doing is setting the input to the state.year
+        chooseColor: (state, action: PayloadAction<string>) => {
+            state.color = action.payload;
+        },
+        chooseMake: (state, action: PayloadAction<string>) => {
+            state.make = action.payload;
+        },
+        chooseModel: (state, action: PayloadAction<string>) => {
+            state.model = action.payload;
+        },
+    },
+});
 
 export const reducer = rootSlice.reducer;
 export const { chooseYear, chooseColor, chooseMake, chooseModel } = rootSlice.actions
